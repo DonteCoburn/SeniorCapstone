7218e08c022aa1ac0665d35292935c60771340af
Donte <coburndonte@gmail.com>
2024-06-13 20:50:57 -0400

    Set fixed Photon region to 'us' to ensure consistency across devices

--- Senior Project Backup copy/Assets/Donte's Stuff/Scripts/ConnectToServer.cs
+++ Senior Project Backup copy/Assets/Donte's Stuff/Scripts/ConnectToServer.cs
@@ -9,8 +9,13 @@ public class ConnectToServer : MonoBehaviourPunCallbacks
     // Start is called before the first frame update
     void Start()
     {
+
         Debug.Log("Attempting to connect to Photon...");
+        PhotonNetwork.PhotonServerSettings.AppSettings.FixedRegion = "us"; //making the region specific to see if it fixing the multiple devices issue -Donte :)
         PhotonNetwork.ConnectUsingSettings();
+        // Old code :(
+        //Debug.Log("Attempting to connect to Photon...");
+        //PhotonNetwork.ConnectUsingSettings();
     }
 
     public override void OnConnectedToMaster()
