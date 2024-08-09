4731c032118668dee1487678975677b3593c932b
Donte <coburndonte@gmail.com>
2024-08-09 13:45:17 -0400

    Fixed player movement bug, where it wouldn't function in singleplayer due to multiplayer functionalities, the player object now works in all environments

--- Senior Project Backup copy/Assets/Sebastian's Stuff/Playermove.cs
+++ Senior Project Backup copy/Assets/Sebastian's Stuff/Playermove.cs
@@ -26,11 +26,18 @@ public class PlayerMovement2 : MonoBehaviourPunCallbacks, IPunObservable
     private void Start()
     {
         view = GetComponent<PhotonView>();
+        if (view == null)
+        {
+            Debug.LogWarning("PhotonView component is missing on this GameObject. Certain network functionalities will not work, this is normal for singleplayer.");
+        }
     }
+
 
     private void Update()
     {
-        if (view.IsMine && canMove)
+        // Check if view is either null, meaning no PhotonView is needed, or view is mine, and canMove is true
+        // Basically this is necessary since sometimes the view component won't exist, like with singleplayer objects
+        if ((view == null || view.IsMine) && canMove)
         {
             if (isDashing)
             {
@@ -57,6 +64,7 @@ public class PlayerMovement2 : MonoBehaviourPunCallbacks, IPunObservable
             Flip();
         }
     }
+
 
     private void FixedUpdate()
     {
@@ -102,17 +110,37 @@ public class PlayerMovement2 : MonoBehaviourPunCallbacks, IPunObservable
     {
         canDash = false;
         isDashing = true;
-        view.RPC("StartDash", RpcTarget.All);
+        // Only send RPC if PhotonView is present (multiplayer setting)
+        if (view != null)
+        {
+            view.RPC("StartDash", RpcTarget.All);
+        }
+        else
+        {
+            StartDash(); // Call locally if no PhotonView (singleplayer setting)
+        }
+
         float originalGravity = rb.gravityScale;
         rb.gravityScale = 0f;
         rb.velocity = new Vector2(transform.localScale.x * dashingPower, 0f);
         yield return new WaitForSeconds(dashingTime);
-        view.RPC("EndDash", RpcTarget.All);
+
+        // Only send RPC if PhotonView is present (multiplayer setting)
+        if (view != null)
+        {
+            view.RPC("EndDash", RpcTarget.All);
+        }
+        else
+        {
+            EndDash(); // Call locally if no PhotonView (singleplayer setting)
+        }
+
         rb.gravityScale = originalGravity;
         isDashing = false;
         yield return new WaitForSeconds(dashingCooldown);
         canDash = true;
     }
+
 
     [PunRPC]
     void StartDash()
@@ -131,249 +159,3 @@ public class PlayerMovement2 : MonoBehaviourPunCallbacks, IPunObservable
         canMove = false;
     }
 }
-
-
-
-/*
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using Photon.Pun;
-
-public class PlayerMovement2 : MonoBehaviour, IPunObservable
-{
-    PhotonView view; //For Photon, do not remove -Donte
-    private float horizontal;
-    private float speed = 8f;
-    private float jumpingPower = 12f;
-    private bool isFacingRight = true;
-
-    private bool canDash = true;
-    private bool isDashing;
-    private bool canMove = true;  // Added to control movement
-    private float dashingPower = 10f;
-    private float dashingTime = 0.5f;
-    private float dashingCooldown = 1f;
-
-    [SerializeField] private Rigidbody2D rb;
-    [SerializeField] private Transform groundCheck;
-    [SerializeField] private LayerMask groundLayer;
-    [SerializeField] private TrailRenderer tr;
-
-    private void Start()
-    {
-        view = GetComponent<PhotonView>();
-    }
-
-    private void Update()
-    {
-        if (view.IsMine && canMove)  // Check if the player can move
-        {
-            if (isDashing)
-            {
-                return;
-            }
-
-            horizontal = Input.GetAxisRaw("Horizontal");
-
-            if (Input.GetButtonDown("Jump") && IsGrounded())
-            {
-                rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
-            }
-
-            if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
-            {
-                rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
-            }
-
-            if (Input.GetKeyDown(KeyCode.LeftShift) && canDash)
-            {
-                StartCoroutine(Dash());
-            }
-
-            Flip();
-        }
-    }
-
-    private void FixedUpdate()
-    {
-        if (isDashing || !canMove)  // Also check canMove here
-        {
-            return;
-        }
-
-        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
-    }
-
-    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
-    {
-        if (stream.IsWriting)
-        {
-            stream.SendNext(transform.position);
-            stream.SendNext(isDashing);
-        }
-        else
-        {
-            transform.position = (Vector3)stream.ReceiveNext();
-            isDashing = (bool)stream.ReceiveNext();
-        }
-    }
-
-    private bool IsGrounded()
-    {
-        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
-    }
-
-    private void Flip()
-    {
-        if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
-        {
-            Vector3 localScale = transform.localScale;
-            isFacingRight = !isFacingRight;
-            localScale.x *= -1f;
-            transform.localScale = localScale;
-        }
-    }
-
-    private IEnumerator Dash()
-    {
-        canDash = false;
-        isDashing = true;
-        float originalGravity = rb.gravityScale;
-        rb.gravityScale = 0f;
-        rb.velocity = new Vector2(transform.localScale.x * dashingPower, 0f);
-        tr.emitting = true;
-        yield return new WaitForSeconds(dashingTime);
-        tr.emitting = false;
-        rb.gravityScale = originalGravity;
-        isDashing = false;
-        yield return new WaitForSeconds(dashingCooldown);
-        canDash = true;
-    }
-
-    public void DisableMovement()
-    {
-        canMove = false;
-    }
-}
-
-/*
-using System.Collections.Generic;
-using System.Collections;
-using UnityEngine;
-using Photon.Pun; //For Photon, do not remove -Donte
-
-public class PlayerMovement2 : MonoBehaviour, IPunObservable
-{
-    PhotonView view; //For Photon, do not remove -Donte
-    private float horizontal;
-    private float speed = 8f;
-    private float jumpingPower = 12f;
-    private bool isFacingRight = true;
-
-    private bool canDash = true;
-    private bool isDashing;
-    private float dashingPower = 10f;
-    private float dashingTime = 0.5f;
-    private float dashingCooldown = 1f;
-
-    [SerializeField] private Rigidbody2D rb;
-    [SerializeField] private Transform groundCheck;
-    [SerializeField] private LayerMask groundLayer;
-    [SerializeField] private TrailRenderer tr;
-
-    private void Start()
-    {
-        view = GetComponent<PhotonView>(); //This makes it so each player only has control of their own character -Donte
-    }
-
-    private void Update()
-    {
-        if (view.IsMine)
-        {
-            if (isDashing)
-            {
-                return;
-            }
-
-            horizontal = Input.GetAxisRaw("Horizontal");
-
-            if (Input.GetButtonDown("Jump") && IsGrounded())
-            {
-                rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
-            }
-
-            if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
-            {
-                rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
-            }
-
-            if (Input.GetKeyDown(KeyCode.LeftShift) && canDash)
-            {
-                StartCoroutine(Dash());
-            }
-
-            Flip();
-        }
-    }
-
-    private void FixedUpdate()
-    {
-        if (isDashing)
-        {
-            return;
-        }
-
-        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
-    }
-
-    //Important Network stuff -Donte
-    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
-    {
-        if (stream.IsWriting)
-        {
-            stream.SendNext(transform.position);
-            stream.SendNext(isDashing);
-        }
-        else
-        {
-            transform.position = (Vector3)stream.ReceiveNext();
-            isDashing = (bool)stream.ReceiveNext();
-        }
-    }
-
-    private bool IsGrounded()
-    {
-        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
-    }
-
-    private void Flip()
-    {
-        if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
-        {
-            Vector3 localScale = transform.localScale;
-            isFacingRight = !isFacingRight;
-            localScale.x *= -1f;
-            transform.localScale = localScale;
-        }
-    }
-
-    private IEnumerator Dash()
-    {
-        canDash = false;
-        isDashing = true;
-        float originalGravity = rb.gravityScale;
-        rb.gravityScale = 0f;
-        rb.velocity = new Vector2(transform.localScale.x * dashingPower, 0f);
-        tr.emitting = true;
-        yield return new WaitForSeconds(dashingTime);
-        tr.emitting = false;
-        rb.gravityScale = originalGravity;
-        isDashing = false;
-        yield return new WaitForSeconds(dashingCooldown);
-        canDash = true;
-    }
-
-
-}
-*/
